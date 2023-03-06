6fa7d56473b332d85a6c413de3612f3b1fcafdbd
Chetan Gupta <chetan.gupta@chicmic.co.in>
2023-03-06 11:25:56 +0530

    Add project files.

---
+++ ChatApplication/Controllers/AuthController.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StudentManagementSystemAPI.Models;
+using StudentManagementSystemAPI.Services;
+
+
+namespace StudentManagementSystemAPI.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        AuthService authService;
+        TeacherService teacherService;
+        Response response = new Response();
+        private readonly ILogger<AuthController> _logger;
+
+        public AuthController(IConfiguration configuration, ILogger<AuthController> logger)
+        {
+            authService = new AuthService(configuration);
+            teacherService = new TeacherService(configuration);
+            _logger = logger;
+        }
+
+        [HttpPost]
+        [Route("/api/v1/RegisterTeacher")]
+        public IActionResult AddTeacher([FromBody] AddTeacher addTeacher)
+        {
+            /*if(!ModelState.IsValid)
+            {
+                response.StatusCode = 400;
+                response.Message = "Invalid input";
+                response.Data = ValidationProblem(ModelState);
+                return BadRequest(response);
+            }*/
+            try
+            {
+                _logger.LogInformation("Add Teacher method started");
+                Response response = teacherService.AddTeacher(addTeacher);
+                return Ok(response);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError("Internal server error something wrong happened ", DateTime.Now);
+                return StatusCode(500, $"Internal server error: {ex}"); ;
+            }
+        }
+
+        [HttpPost("TeacherLogin")]
+        public ActionResult<User> TeacherLogin(UserDTO request)
+        {
+            _logger.LogInformation("Teacher Login attempt");
+            response = authService.TeacherLogin(request);
+
+            if (response.StatusCode == 404)
+            {
+                return BadRequest(response);
+            }
+            else if (response.StatusCode == 403)
+            {
+                return BadRequest(response);
+            }
+            return Ok(response);
+        }
+    }
+}
+   
---
+++ ChatApplication/Program.cs
@@ -0,0 +1,83 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.Extensions.FileProviders;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.Filters;
+using System.Text;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.AddSecurityDefinition("oauth2", new OpenApiSecurityScheme
+    {
+        Description = "Standard Authorization header using the Bearer scheme (\"bearer {token}\")",
+        In = ParameterLocation.Header,
+        Name = "Authorization",
+        Type = SecuritySchemeType.ApiKey
+    });
+    options.OperationFilter<SecurityRequirementsOperationFilter>();
+});
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+        .AddJwtBearer(options =>
+        {
+            options.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters
+            {
+                ValidateIssuerSigningKey = true,
+                IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8
+                .GetBytes(builder.Configuration.GetSection("AppSettings:Token").Value!)),
+                ValidateLifetime = true,
+                ValidateIssuer = false,
+                ValidateAudience = false,
+                ClockSkew = TimeSpan.Zero
+            };
+        });
+
+builder.Services.AddCors(options => options.AddPolicy(name: "CorsPolicy",
+    policy =>
+    {
+        policy.WithOrigins().AllowAnyMethod().AllowAnyHeader();
+    }
+    ));
+
+/*builder.Services.AddScoped<IAuthService, AuthService>();
+builder.Services.AddScoped<ITeacherService, TeacherService>();
+builder.Services.AddScoped<IStudentService, StudentService>();
+builder.Services.AddScoped<IUploadPicService, UploadPicService>();
+*/
+// Build the WebApplication instance
+var app = builder.Build();
+
+// If the environment is development, use Swagger and SwaggerUI
+
+app.UseSwagger();
+app.UseSwaggerUI();
+
+
+// Use HTTPS redirection
+app.UseHttpsRedirection();
+
+app.UseStaticFiles(new StaticFileOptions
+{
+    FileProvider = new PhysicalFileProvider(
+           Path.Combine(builder.Environment.ContentRootPath, "Assets")),
+    RequestPath = "/StaticFiles"
+});
+
+app.UseRouting();
+
+app.UseCors("CorsPolicy");
+
+app.UseAuthentication();
+
+// Use authentication and authorization
+app.UseAuthorization();
+// Map the controllers
+app.MapControllers();
+// Run the application
+app.Run();
